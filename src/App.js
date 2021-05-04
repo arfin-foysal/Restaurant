556b339a75f0a9eb7025455f7c9f07cdc98a0c93
Arfin Foysal <arfinfoysalbd@gmail.com>
2021-05-04 22:49:32 +0600

    component tree

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import MainComponents from './components/MainComponents'
 
 function App() {
   return (
     <div className="App">
      
+      <MainComponents/>
     </div>
   );
 }
---
+++ src/components/MainComponents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+import Body from './body/Body'
+
+export default function MainComponents() {
+    return (
+        <div>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    )
+}
